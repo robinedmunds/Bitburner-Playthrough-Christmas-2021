43b7add8c56ee1b6faeef2270ea3c4489993d7e0
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-12-30 15:37:12 +0000

    Disable log flooding in controller/main.js

--- controller/main.js
+++ controller/main.js
@@ -2,6 +2,8 @@
 import { Controller } from "/scripts/controller/lib/Controller.js"
 
 const main = async (ns) => {
+  await ns.disableLog("ALL")
+
   let controller = null
   while (true) {
     controller = new Controller(ns)
