5f7666da334f8d1b51c12be2e342f3398c911a85
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2022-01-04 00:48:32 +0000

    Refactor route util. Now outputs connect cmd for copy/paste

--- utils/route.js
+++ utils/route.js
@@ -2,29 +2,35 @@
 import { AllNodes } from "/scripts/hacking/lib/AllNodes.js"
 
 const main = (ns) => {
-  const arg = ns.args[0] || "run4theh111z"
+  const arg = ns.args[0]
   const allNodesObj = new AllNodes(ns, "home")
   const routes = allNodesObj.routes
 
-  ns.tprint("**********************")
-  ns.tprint("***  ROUTE FINDER  ***")
-  ns.tprint("**********************")
-  ns.tprint("")
-  ns.tprint("Type server name as first argument or use --all")
-  ns.tprint("")
-
-  if (arg !== "--all") {
-    if (!ns.serverExists(arg)) {
-      ns.tprint(`ERROR:  ${arg} is unreachable.`)
-      return
-    }
-    const route = routes[arg].join(" -> ")
-    ns.tprint(`INFO:  ${route}`)
-  } else if (arg === "--all") {
+  if (arg === "--all") {
     for (const [key, route] of Object.entries(routes)) {
       const formatted = route.join(" -> ")
       ns.tprint(`INFO:  ${key}:  ${formatted}`)
     }
+  }
+
+  if (Object.keys(allNodesObj.nodes).includes(arg)) {
+    const route = routes[arg].join(" -> ")
+    const chainedCmd = routes[arg]
+      .filter((i) => i !== "home")
+      .map((i) => `connect ${i}`)
+      .join(" ; ")
+
+    ns.tprint(`INFO:  ${route}`)
+    ns.tprint("WARNING: ", chainedCmd)
+  }
+
+  if (!arg) {
+    ns.tprint("**********************")
+    ns.tprint("***  ROUTE FINDER  ***")
+    ns.tprint("**********************")
+    ns.tprint("")
+    ns.tprint("Type server name as first argument or use --all")
+    ns.tprint("")
   }
 }
 
